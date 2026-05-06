8b61118099fa0e520f50f4db757abd35f23928f8
5Leal30 <sebastianleal583@gmail.com>
2026-05-06 18:11:21 -0500

    Update Acciones_referenciales.sql

--- Ciclo1/ACCIONES_REF/Acciones_referenciales.sql
+++ Ciclo1/ACCIONES_REF/Acciones_referenciales.sql
@@ -1,21 +1,9 @@
-ALTER TABLE Compradores ADD CONSTRAINT fk_comprador_Personas FOREIGN KEY (idPersona) REFERENCES Personas(idPersona) ON DELETE CASCADE;
-ALTER TABLE Vendedores ADD CONSTRAINT fk_vendedor_Personas FOREIGN KEY (idPersona) REFERENCES Personas(idPersona) ON DELETE CASCADE;
-ALTER TABLE Homologaciones ADD CONSTRAINT fk_Homologaciones_auto FOREIGN KEY (idAuto) REFERENCES Automoviles(idAutomovil) ON DELETE CASCADE;
-ALTER TABLE Inventarios ADD CONSTRAINT fk_inventario_pieza FOREIGN KEY (idPieza) REFERENCES Piezas(idPieza) ON DELETE CASCADE;
-ALTER TABLE Inventarios ADD CONSTRAINT fk_inventario_Talleres FOREIGN KEY (idTaller) REFERENCES Talleres(idTaller) ON DELETE CASCADE;
-ALTER TABLE HistorialMantenimientos ADD CONSTRAINT fk_mant_automovil FOREIGN KEY (idAutomovil) REFERENCES Automoviles(idAutomovil) ON DELETE CASCADE;
-ALTER TABLE HistorialMantenimientos ADD CONSTRAINT fk_mant_Talleres FOREIGN KEY (idTaller) REFERENCES Talleres(idTaller) ON DELETE CASCADE;
-ALTER TABLE Contienen ADD CONSTRAINT fk_Contienen_pedido FOREIGN KEY (idPedido) REFERENCES Pedidos(idPedido) ON DELETE CASCADE;
-ALTER TABLE Contienen ADD CONSTRAINT fk_Contienen_pieza FOREIGN KEY (idPieza) REFERENCES Piezas(idPieza) ON DELETE CASCADE;
-ALTER TABLE CarrosInteresado ADD CONSTRAINT fk_carrosinteresado_comprador FOREIGN KEY (idPersona) REFERENCES Compradores(idPersona) ON DELETE CASCADE;
-ALTER TABLE PiezasInteresado ADD CONSTRAINT fk_piezasinteresado_comprador FOREIGN KEY (idPersona) REFERENCES Compradores(idPersona) ON DELETE CASCADE;
-ALTER TABLE CarrosComprados ADD CONSTRAINT fk_carroscomprados_comprador FOREIGN KEY (idPersona) REFERENCES Compradores(idPersona) ON DELETE CASCADE;
-ALTER TABLE CarrosOfrece ADD CONSTRAINT fk_carrosofrece_vendedor FOREIGN KEY (idPersona) REFERENCES Vendedores(idPersona) ON DELETE CASCADE;
-ALTER TABLE AutosCompatibles ADD CONSTRAINT fk_autoscompatibles_pieza FOREIGN KEY (idPieza) REFERENCES Piezas(idPieza) ON DELETE CASCADE;
-ALTER TABLE CompraAutos ADD CONSTRAINT fk_CompraAutos_compra FOREIGN KEY (idCompra) REFERENCES Compras(idCompra) ON DELETE CASCADE;
-ALTER TABLE CompraAutos ADD CONSTRAINT fk_CompraAutos_auto FOREIGN KEY (idAuto) REFERENCES Automoviles(idAutomovil) ON DELETE CASCADE;
-ALTER TABLE CompraPiezas ADD CONSTRAINT fk_CompraPiezas_compra FOREIGN KEY (idCompra) REFERENCES Compras(idCompra) ON DELETE CASCADE;
-ALTER TABLE CompraPiezas ADD CONSTRAINT fk_CompraPiezas_pieza FOREIGN KEY (idPieza) REFERENCES Piezas(idPieza) ON DELETE CASCADE;
+ALTER TABLE Homologaciones ADD CONSTRAINT fk_homologaciones_automovil FOREIGN KEY (idAuto) REFERENCES Automoviles(idAutomovil) ON DELETE CASCADE;
+ALTER TABLE Facturas ADD CONSTRAINT fk_facturas_compra FOREIGN KEY (idCompra) REFERENCES Compras(idCompra) ON DELETE CASCADE;
+ALTER TABLE Pedidos ADD CONSTRAINT fk_pedidos_proveedor FOREIGN KEY (idProveedor) REFERENCES Proveedores(idProveedor) ON DELETE CASCADE;
+ALTER TABLE TramitesAduanas ADD CONSTRAINT fk_tramites_importacion FOREIGN KEY (idImportacion) REFERENCES Importaciones(idImportacion) ON DELETE CASCADE;
+ALTER TABLE TramitesAduanas ADD CONSTRAINT fk_tramites_pedido FOREIGN KEY (idPedido) REFERENCES Pedidos(idPedido) ON DELETE CASCADE;
+ALTER TABLE Inventarios ADD CONSTRAINT fk_inventarios_taller FOREIGN KEY (idTaller) REFERENCES Talleres(idTaller) ON DELETE CASCADE;
 
 --Restricciones de Tuplas
 
