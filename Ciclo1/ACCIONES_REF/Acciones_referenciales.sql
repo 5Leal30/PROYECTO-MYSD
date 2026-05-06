1d92cb755012aaf67ca95bb75a224727af2dd268
5Leal30 <sebastianleal583@gmail.com>
2026-05-05 21:11:04 -0500

    Update Restricciones de Tupla Acciones_referenciales.sql

--- Ciclo1/ACCIONES_REF/Acciones_referenciales.sql
+++ Ciclo1/ACCIONES_REF/Acciones_referenciales.sql
@@ -16,3 +16,49 @@ ALTER TABLE CompraAutos ADD CONSTRAINT fk_CompraAutos_compra FOREIGN KEY (idComp
 ALTER TABLE CompraAutos ADD CONSTRAINT fk_CompraAutos_auto FOREIGN KEY (idAuto) REFERENCES Automoviles(idAutomovil) ON DELETE CASCADE;
 ALTER TABLE CompraPiezas ADD CONSTRAINT fk_CompraPiezas_compra FOREIGN KEY (idCompra) REFERENCES Compras(idCompra) ON DELETE CASCADE;
 ALTER TABLE CompraPiezas ADD CONSTRAINT fk_CompraPiezas_pieza FOREIGN KEY (idPieza) REFERENCES Piezas(idPieza) ON DELETE CASCADE;
+
+--Restricciones de Tuplas
+
+ALTER TABLE Compras
+ADD CONSTRAINT check_compras_fechas 
+CHECK (
+    (fechaEntrega IS NULL OR fechaEntrega >= fechaCompra)
+    AND
+    (estadoPago = 'Realizado' OR fechaEntrega IS NULL)
+);
+
+ALTER TABLE Vendedores
+ADD CONSTRAINT check_vendedores_concesionario 
+CHECK (
+    (esConcecionario = 'Si' AND nombreConcecionario IS NOT NULL) 
+    OR 
+    (esConcecionario = 'No' AND nombreConcecionario IS NULL)
+);
+
+ALTER TABLE Pedidos
+ADD CONSTRAINT check_pedidos_fechas 
+CHECK (fechaPedido < fechaEntregaEstimada);
+
+
+ALTER TABLE TramitesAduanas
+ADD CONSTRAINT check_tramites_fechas 
+CHECK (fechaInicio < fechaFin);
+
+
+ALTER TABLE Importaciones
+ADD CONSTRAINT check_importaciones_fechas 
+CHECK (fechaSalida < fechaLlegada);
+
+
+ALTER TABLE Importaciones
+ADD CONSTRAINT check_importaciones_puertos 
+CHECK (puertoSalida <> puertoLlegada);
+
+
+ALTER TABLE Homologaciones
+ADD CONSTRAINT check_homologaciones_fechas 
+CHECK (
+    fechaSolicitud <= fechaAprobacion
+    AND
+    fechaVigenciaHasta > fechaAprobacion
+);
